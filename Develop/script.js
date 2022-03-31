6ebd03775b3e73a69f43abc945bb111dfd159032
Hannah Lodge <lodg0021@umn.edu>
2022-03-31 11:34:35 -0500

    changed the file structure

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,22 @@
-var present = moment().format("hA");
+$(".time-block").removeClass("future");
+$(".time-block").removeClass("past");
+$(".time-block").removeClass("present");
+
+var present = moment().format("h");
+
+if (present < 9) {
+    $(".time-block").addClass("future");
+}
+else if (present > 18) {
+    $(".time-block").addClass("past");
+} else {
+    var present_id = moment().format("#hA");
+
+    $(present_id).addClass("present");
+    $(present_id).nextAll().addClass("future");
+    $(present_id).prevAll().addClass("past");
+}
+
+$(".saveBtn").on("click", function(e) {
+   console.log(this); 
+});
