ecc3d0d43150bbaf6c5c4de1e4ce174460795465
Hannah Lodge <lodg0021@umn.edu>
2022-03-31 15:24:30 -0500

    Save and retrieve from localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,3 @@
-// $(".time-block").removeClass("future");
-// $(".time-block").removeClass("past");
-// $(".time-block").removeClass("present");
-
 var present = moment().format("H");
 var currentDate= moment().format("dddd, MMMM Do");
 var dateSpace=$("#currentDay");
@@ -23,9 +19,31 @@ else if (present > 18) {
     $(present_id).prevAll().addClass("past");
 }
 
+// Repopulate textarea from local storage
+var start_time = 9;
+var end_time = 18;
+t = start_time;
+while(t<end_time) {
+    ampm = "AM";
+    ampm_t=t;
+    if(t>=12){
+        ampm = "PM";
+        if(t>12){
+           ampm_t = t-12;
+        }
+    }
+    var t_ampm = ampm_t+ampm;
+    var descr = localStorage.getItem(t_ampm);
+    if (descr!=null) {
+        $('#'+t_ampm+' textarea').val(descr);
+    }
+    t++;
+}
+
 // On clicking the same button, get the text in the corrosponding text area
 $(".saveBtn").on("click", function(e) {
     var description = $(this).parent().find('.description');
     var text = description.val();
     var hour = description.attr('name');
+    localStorage.setItem(hour, text);
 });
